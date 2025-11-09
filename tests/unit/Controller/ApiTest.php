f26e353660d129c1290274ba2561ee36502f6e3f
Chen Asraf <casraf@pm.me>
2025-11-09 02:16:29 +0200

    fix: flexible test-docker target, add working ApiController test

--- tests/unit/Controller/ApiTest.php
+++ tests/unit/Controller/ApiTest.php
@@ -6,24 +6,69 @@ namespace Controller;
 
 use OCA\NextcloudAppTemplate\AppInfo\Application;
 use OCA\NextcloudAppTemplate\Controller\ApiController;
-use OCA\NextcloudAppTemplate\Service\FetchCurrenciesService;
 use OCP\IAppConfig;
 use OCP\IL10N;
 use OCP\IRequest;
 use PHPUnit\Framework\TestCase;
 
 class ApiTest extends TestCase {
-	public function testIndex(): void {
+	public function testGetHello(): void {
 		$request = $this->createMock(IRequest::class);
 		$config = $this->createMock(IAppConfig::class);
-		$l = $this->createMock(IL10N::class);
-		$service = $this->createMock(FetchCurrenciesService::class);
+		$l10n = $this->createMock(IL10N::class);
 
-		$controller = new ApiController(Application::APP_ID, $request, $config, $l, $service);
+		// Mock config to return empty string (no previous hello)
+		$config->method('getValueString')
+			->willReturn('');
 
-		$resp = $controller->getCronInfo()->getData();
-		echo json_encode($resp);
-		$this->assertEquals(null, $resp['last_update']);
-		$this->assertEquals(0, $resp['interval']);
+		// Mock translation to return a simple string
+		$l10n->method('t')
+			->willReturnCallback(function ($text, $params = []) {
+				return $text;
+			});
+
+		$controller = new ApiController(Application::APP_ID, $request, $config, $l10n);
+
+		$resp = $controller->getHello()->getData();
+
+		$this->assertIsArray($resp);
+		$this->assertArrayHasKey('message', $resp);
+		$this->assertArrayHasKey('at', $resp);
+		$this->assertEquals('👋 Hello from server!', $resp['message']);
+		$this->assertNull($resp['at']);
+	}
+
+	public function testPostHello(): void {
+		$request = $this->createMock(IRequest::class);
+		$config = $this->createMock(IAppConfig::class);
+		$l10n = $this->createMock(IL10N::class);
+
+		// Mock translation to return formatted string
+		$l10n->method('t')
+			->willReturnCallback(function ($text, $params = []) {
+				if (empty($params)) {
+					return $text;
+				}
+				return vsprintf(str_replace('%s', '%s', $text), $params);
+			});
+
+		// Expect setValueString to be called to save the timestamp
+		$config->expects($this->once())
+			->method('setValueString');
+
+		$controller = new ApiController(Application::APP_ID, $request, $config, $l10n);
+
+		$resp = $controller->postHello([
+			'name' => 'World',
+			'theme' => 'dark',
+			'items' => ['item1', 'item2'],
+			'counter' => 5
+		])->getData();
+
+		$this->assertIsArray($resp);
+		$this->assertArrayHasKey('message', $resp);
+		$this->assertArrayHasKey('at', $resp);
+		$this->assertStringContainsString('World', $resp['message']);
+		$this->assertNotEmpty($resp['at']);
 	}
 }
